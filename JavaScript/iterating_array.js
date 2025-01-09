ff441421c5a17b609823de2d1db2e772ccca7253
elainexu15 <elainexu15@gmail.com>
2025-01-09 14:45:29 +1300

    HTML iframe

---
+++ JavaScript/iterating_array.js
@@ -0,0 +1,11 @@
+/*
+Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?
+Сan you describe other popular methods for iterating over arrays?
+
+Array.forEach() loop and Array.map() methods are both used to iterate over arrays, but they have some key differences.
+Array.forEach() loop is used to execute a function on each element of an array, but it does not return a new array. It simply executes the function for each element in the array.
+
+Other Popular Methods for Iterating Over Arrays
+
+
+*/
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Coding-Everyday
+
+https://github.com/h5bp/Front-end-Developer-Interview-Questions/blob/main/src/questions/javascript-questions.md
